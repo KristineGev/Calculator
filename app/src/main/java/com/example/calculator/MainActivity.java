6902ada69cdfbed4fe1c0bdb95e2f5824107a059
KristineGev <christigevorgyan@yahoo.com>
2019-03-10 00:31:52 +0400

    first commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,128 +2,137 @@ package com.example.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button numberButton, signButton, operationButton;
-    TextView resultDisplay, numberDisplay;
-    String resultField = "";
-    String operationField = "";
+    Button numberButton, operationButton;
+    TextView resultDisplay, operationDisplay;
+    String numberField = "";
+    String operations = "";
     String operator = "";
     String lastOperator = "";
+    boolean isPercentClicked = false;
 
-    Double firstNumber = 0.00;
-    Double secondNumber = 0.00;
-    Double result = 0.00;
+    Double firstNumber = 0.0;
+    Double secondNumber = 0.0;
+    Double result = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        resultDisplay.setText("0");
     }
 
     public void init() {
         resultDisplay = findViewById(R.id.display_result);
-        numberDisplay = findViewById(R.id.dislpay_number);
+        operationDisplay = findViewById(R.id.dislpay_number);
     }
 
     public void onNumberButtonClick(View v) {
         numberButton = (Button) v;
-        resultField += numberButton.getText();
-        resultDisplay.setText(resultField);
+        numberField += numberButton.getText();
+        resultDisplay.setText(numberField);
     }
 
     public void onOperationClick(View v) {
-        if (resultField != "") {
-            firstNumber = Double.parseDouble(resultField);
+        if (numberField != "") {
+            firstNumber = Double.parseDouble(numberField);
+
         }
         operator = ((Button) v).getText().toString();
-        operationField += operator + firstNumber;
-        numberDisplay.setText(operationField);
-        resultField = "";
+        operations += firstNumber + operator;
+        operationDisplay.setText(operations);
+        numberField = "";
         calculate(v);
         lastOperator += operator;
     }
 
     public void calculate(View v) {
-        if (resultField == "") {
+        if (numberField == "") {
             if (result == 0) {
                 secondNumber = result;
                 result = firstNumber;
+            } else {
+                secondNumber = firstNumber;
+                firstNumber = result;
             }
-            secondNumber = firstNumber;
-            firstNumber = result;
-        } else
-            {
-            secondNumber = Double.parseDouble(resultField);
-            operationField += secondNumber;
+
+        } else {
+            secondNumber = Double.parseDouble(numberField);
+            operations += secondNumber;
         }
-        resultDisplay.setText(resultField);
-        resultField = "";
+        resultDisplay.setText(numberField);
+        numberField = "";
 
         if (lastOperator.length() > 0) {
             switch (lastOperator.substring(lastOperator.length() - 1)) {
                 case "+":
                     result += secondNumber;
-                    Log.v(operator, operationField + "");
-                    resultField = result.toString();
                     break;
                 case "-":
                     result -= secondNumber;
-                    resultDisplay.setText(String.valueOf(result));
-                    resultField = result.toString();
-                    Log.v("Tag", "first " + firstNumber + " second " + secondNumber + " result " + result);
                     break;
                 case "*":
                     result *= secondNumber;
-                    resultDisplay.setText(String.valueOf(result));
                     break;
                 case "/":
                     result /= secondNumber;
-                    resultDisplay.setText(String.valueOf(result));
                     break;
-                case "=":
-                    resultField = "";
-                    operationField="";
-                    numberDisplay.setText("");
-                    resultDisplay.setText("");
+                default:
                     break;
             }
         }
-        numberDisplay.setText(operationField);
-        resultField = "";
 
+        numberField = result.toString();
+        operationDisplay.setText(operations);
+        numberField = "";
         resultDisplay.setText(String.valueOf(result));
     }
 
     public void onPercentClick(View v) {
-        if (secondNumber != 0.00) {
-            secondNumber /= 100;
-        } else {
-            firstNumber /= 100;
-        }
+
+    }
+
+
+    public void onEqualClick(View v) {
+        calculate(v);
+        operations = "";
+        numberField = "";
+        operationDisplay.setText("");
+        resultDisplay.setText(String.valueOf(result));
+        firstNumber = result;
+        result = 0.00;
+        lastOperator = "";
+        isPercentClicked = false;
+
+    }
+
+    public void onSignButtonClick(View v) {
+        Double res = Double.parseDouble(numberField) * -1;
+        resultDisplay.setText(String.valueOf(res));
+        numberField = String.valueOf(res);
     }
 
     public void onDeleteClick(View v) {
         String display = resultDisplay.getText().toString();
         if (display.length() > 0) {
-            resultField = display.substring(0, display.length() - 1);
-            resultDisplay.setText(resultField);
+            numberField = display.substring(0, display.length() - 1);
+            resultDisplay.setText(numberField);
         } else
             resultDisplay.setText("0");
     }
 
     public void onClear(View view) {
         operationButton = (Button) view;
-        resultField = "";
-        operationField = "";
+        numberField = "";
+        operations = "";
         resultDisplay.setText("0");
-        numberDisplay.setText("");
+        operationDisplay.setText("");
         result = 0.00;
         secondNumber = 0.00;
         lastOperator = "";
